5c6063b7bafd5044235dda0359343569dd976157
Yiren Jian <yirenjian0621@gmail.com>
2023-07-10 01:50:26 -0400

    update

--- Video_Caption/msrvtt_pycocoeval_annotations.py
+++ Video_Caption/msrvtt_pycocoeval_annotations.py
@@ -2,7 +2,7 @@ import os
 import json
 
 original_annotation_file = "/home/yiren/lavis_datasets/msrvtt/annotations/cap_test.json"
-new_annotation_file = "/home/yiren/lavis_datasets/msrvtt_gt/cap_test.json"
+new_annotation_file = "/home/yiren/lavis_datasets/msrvtt_gt/msrvtt_cap_test.json"
 
 with open(original_annotation_file) as f:
     original_annotation = json.load(f)
